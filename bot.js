420d4ab76662e38b8ffc1197990ae8244af80729
IcedCrescent <35650172+IcedCrescent@users.noreply.github.com>
2020-07-17 01:42:31 +0700

    First commit

---
+++ bot.js
@@ -0,0 +1,82 @@
+const Discord = require("discord.js");
+const PREFIX = ':'
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+let bot = new Discord.Client();
+let fortunes = [
+    "không", "đoán xem", "tôi không nghe, tôi không thấy", "đi ngủ để mer đi", "mẹ cái bọn lesor", "thằng đầu buổi giẻ rách"
+]
+let bully = [
+    "thằng đầu buồi giẻ rách này", "nghe mà đau nát trái tim này", "mẹ cái bọn lesor", "NamKey buscu Backy", "đừng chửi em nữa, yêu em đi", "hic cai loz, im lang", "ngoan ngoan", "nếu cảm thấy mệt quá, em cho mượn bờ vide, thì thầm em nói nhỏ, tổng cộng năm trăm hai",
+    "hic là năm lít"
+]
+let fatdean = [
+    "dai ja xao loz", "mập quá anh ơi", "ai nớp diu chu cà mo"
+]
+fatto = [
+    "admin cute hehe", "cali khat' nuoc'", "đưa ba trăm để có thêm", "vippro"
+]
+let mk = "cho 300k de co command minh khai"
+
+bot.on('ready', function () {
+    console.log("bot is now online");
+})
+
+bot.on("message", async message => {
+
+    if (message.content === "gioi") {
+        message.channel.send("chu ngu nhu may sao")
+    }
+
+    if (message.content === "twm") {
+        message.channel.send("thomng mimnh sieu cap")
+    }
+
+    if (message.content === "mk") {
+        message.channel.send(mk)
+    }
+
+    if (message.content === "hic") {
+        message.channel.send(bully[Math.floor(Math.random() * bully.length)]);
+    }
+
+    if (message.content === "fatto") {
+        message.channel.send(fatto[Math.floor(Math.random() * fatto.length)]);
+    }
+
+    if (message.content === "fat dean") {
+        message.channel.send(fatdean[Math.floor(Math.random() * fatdean.length)]);
+    }
+
+    if (message.author.equals(bot.user)) return;
+    var args = message.content.substring(PREFIX.length).split(" ")
+    if (!message.content.startsWith(PREFIX)) return;
+    switch (args[0].toLowerCase()) {
+        case "alo": {
+            message.channel.send("công an đây địt mẹ mày");
+            break;
+        }
+
+        case "seno": {
+            message.channel.send("demo sonna");
+            break;
+        }
+        case "help": {
+            message.channel.send("Tôi là con bot vô dụng, tôi không giúp được ai cả hic. Nhưng chửi nhau set ke`o dam' nhau thì oce (° ͜ʖ ͡°)");
+            break;
+        }
+        case "say": {
+            if (args[0]) {
+                message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+            }
+            break;
+        }
+    }
+})
+
+bot.login(process.env.BOT_TOKEN);
+
+
