239d02d782a3fa3fe1d2e126dd2c3de302c1d5d8
nhutam03 <68271877+nhutam03@users.noreply.github.com>
2020-07-19 19:06:34 +0700

    update bot

--- bot.js
+++ bot.js
@@ -1,35 +1,25 @@
 const Discord = require("discord.js");
 const PREFIX = ':'
-
+const fortunes = [ "không", "đoán xem", "tôi không nghe, tôi không thấy", "đi ngủ để mer đi", "mẹ cái bọn lesor", "thằng đầu buổi giẻ rách", "cut' me may di"]
+const randomfortunes = fortunes[Math.floor(Math.random() * fortunes.length)]
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 let bot = new Discord.Client();
-let fortunes = [
-    "không", "đoán xem", "tôi không nghe, tôi không thấy", "đi ngủ để mer đi", "mẹ cái bọn lesor", "thằng đầu buổi giẻ rách", "cut' me may di"
-]
-let bully = [
-    "thằng đầu buồi giẻ rách này", "nghe mà đau nát trái tim này", "mẹ cái bọn lesor", "NamKey buscu Backy", "đừng chửi em nữa, yêu em đi", "hic cai loz, im lang", "ngoan ngoan", "nếu cảm thấy mệt quá, em cho mượn bờ vide, thì thầm em nói nhỏ, tổng cộng năm trăm hai",
-    "hic là năm lít", "Ai là đứa trẻ ngoan nàoooo? Đéo phải mày, cút.", "wjbu thì im mồm", "Phải tôi tôi lại đấm cho mấy fat",
-    "bớt đùa đi, dạo này tao không còn hiền nữa đâu", "yêu em đừng để em buồn, em buồn anh vẫn phải yêu em <3", "tự làm bot mà chơi, chơi chùa là 300k"
-]
-let fatdean = [
-    "dai ja xao loz", "mập quá anh ơi", "thích tò mò không mà kêu hoài z", "an cut’ an db",
-]
-fatto = [
-    "admin cute hehe", "cali khat' nuoc'", "baka bakaaa", "vippro",
-    "phải luôn vui vẻ và tươi trẻ he he"
-]
-let gn = [
-    "Chúc bé ngủ ngon", "oyasumi 😴", "đi ngủ hoặc bị đánh đít", "cút cút mau"
-]
-let mk = [
-    "Gia Lai thich nhau lai rai", "nhớ đốt phong long nha mk" 
-]
-let theanh = [
-    "Đi ăn chè thái hok?", "mẹ cái thằng simp đội loz lên đầu", "duy trì tình bạn bằng 100k bạn nhé"
-]
+
+const bully = [ "thằng đầu buồi giẻ rách này", "nghe mà đau nát trái tim này", "mẹ cái bọn lesor", "NamKey buscu Backy", "đừng chửi em nữa, yêu em đi", "hic cai loz, im lang", "ngoan ngoan", "nếu cảm thấy mệt quá, em cho mượn bờ vide, thì thầm em nói nhỏ, tổng cộng năm trăm hai", "hic là năm lít", "Ai là đứa trẻ ngoan nàoooo? Đéo phải mày, cút.", "wjbu thì im mồm", "Phải tôi tôi lại đấm cho mấy fat", "bớt đùa đi, dạo này tao không còn hiền nữa đâu", "yêu em đừng để em buồn, em buồn anh vẫn phải yêu em <3", "tự làm bot mà chơi, chơi chùa là 300k"]
+const randombully = bully[Math.floor(Math.random() * bully.length)]
+const fatdean = ["dai ja xao loz", "mập quá anh ơi", "thích tò mò không mà kêu hoài z", "an cut’ an db"]
+const randomfatdean = fatdean[Math.floor(Math.random() * fatdean.length)]
+const fatto = ["admin cute hehe", "cali khat' nuoc'", "baka bakaaa", "vippro", "phải luôn vui vẻ và tươi trẻ he he"]
+const randomfatto = fatto[Math.floor(Math.random() * fatto.length)]
+const gn = ["Chúc bé ngủ ngon", "oyasumi 😴", "đi ngủ hoặc bị đánh đít", "cút cút mau"]
+const randomgn = gn[Math.floor(Math.random() * gn.length)]
+const mk = ["Gia Lai thich nhau lai rai", "nhớ đốt phong long nha mk", "ko muon noi gi"]
+const randommk = mk[Math.floor(Math.random() * mk.length)]
+const theanh = ["Đi ăn chè thái hok?", "mẹ cái thằng simp đội loz lên đầu", "duy trì tình bạn bằng 100k bạn nhé"]
+const randomtheanh = theanh[Math.floor(Math.random() * theanh.length)]
 
 bot.on('ready', function () {
     console.log("bot is now online");
@@ -39,7 +29,7 @@ bot.on("message", async message => {
    
 
     if (message.content === "gioi") {
-        message.channel.send("chu ngu nhu may sao")
+        message.channel.send("đệ tự chị twm mà chả thế, chứ đâu ngu như doge ngoo")
     }
 
     if (message.content === "twm") {
@@ -55,15 +45,11 @@ bot.on("message", async message => {
     }
     
     if (message.content === "minh thu") {
-        message.channel.send("j loli trong truyen thuyet sao")
+        message.channel.send("hung du qua do nka")
     }
     
     if (message.content === "hoa cai") {
         message.channel.send("sống ở đời nói phải giữ lời, đừng như con cu biết bay 👌")
-    }
-    
-    if (message.content === "mina") {
-        message.channel.send("hen cac ban vao 20/7")
     }
     
     if (message.content === "nhi") {
@@ -75,27 +61,34 @@ bot.on("message", async message => {
     }
 
     if (message.content === "mk") {
-        message.channel.send(mk[Math.floor(Math.random() * mk.length)]);
+        message.channel.send(randommk);
     }
 
     if (message.content === "hic") {
-        message.channel.send(bully[Math.floor(Math.random() * bully.length)]);
+        message.channel.send(randombully);
     }
 
     if (message.content === "fatto") {
-        message.channel.send(fatto[Math.floor(Math.random() * fatto.length)]);
+        message.channel.send(randomfatto);
     }
 
     if (message.content === "fat dean") {
-        message.channel.send(fatdean[Math.floor(Math.random() * fatdean.length)]);
-    }
-
-    if (message.content.includes === "ngu ngon") {
-        message.channel.send(gn[Math.floor(Math.random() * gn.length)]);
+        message.channel.send(randomfatdean);
     }
 
     if (message.content === "the anh") {
-        message.channel.send(theanh[Math.floor(Math.random() * theanh.length)]);
+        message.channel.send(randomtheanh);
+    }
+
+    if (message.content.includes("ngu ngon")) {
+        message.channel.send(randomgn);
+    }
+
+    if(message.content.startsWith(":say")) {
+        var text = message.content.split(' ').slice(1).join(' ');
+        if(!text) return message.reply("địt mẹ mày sai rồi Ex: :say sủa concac");
+         message.channel.send(text)
+         message.delete();
     }
     
 
@@ -116,7 +109,7 @@ bot.on("message", async message => {
             message.channel.send("Tôi là con bot vô dụng, tôi không giúp được ai cả hic. Nhưng chửi nhau set ke`o dam' nhau thì oce (° ͜ʖ ͡°)");
             break;
         }
-        case "say": {
+        case "talk": {
             if (args[0]) {
                 message.channel.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
             }
